629e554505557764fb8828d23d5d38a29f73c282
John <john@Zipcoders-MacBook-Pro-46.local>
2022-12-04 17:27:22 -0500

    finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,35 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int forwardCounter = 0;
+        int reverseCounter = 0;
+        for (int i = startIndex; i < playList.length; i++) {
+            if (playList[i].equals(selection)) {
+                break;
+            }
+            else {
+                forwardCounter++;
+            }
+            if(i == playList.length - 1) {
+                i = -1;
+            }
+        }
+        for (int i = startIndex; i >= 0; i--) {
+            if (playList[i].equals(selection)) {
+                break;
+            }
+            else {
+                reverseCounter++;
+            }
+            if(i == 0) {
+                i = playList.length;
+            }
+        }
+        if (forwardCounter > reverseCounter) {
+            return reverseCounter;
+        }
+        else {
+            return forwardCounter;
+        }
     }
 }
